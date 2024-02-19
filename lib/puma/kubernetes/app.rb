9832813143ec10830b3c453ab998c0059fc9c0d8
Wagner Caixeta <wagner@baladapp.com.br>
2024-02-19 14:40:10 -0300

    Avoiding errors during puma startup in non web instance

--- lib/puma/kubernetes/app.rb
+++ lib/puma/kubernetes/app.rb
@@ -54,7 +54,7 @@ module Puma
       end
 
       def generate_extended_puma_stats
-        puma_stats = @launcher.stats
+        puma_stats = @launcher&.stats || {}
         # On puma <= 4 puma_stats is a String
         puma_stats = JSON.parse(puma_stats, symbolize_names: true) unless puma_stats.is_a?(Hash)
         # Including usage stats.
