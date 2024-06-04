8d017135c748e6a688ec0d4a9d396db89e3d3e19
Wagner Caixeta <wagner@baladapp.com.br>
2024-06-04 15:01:57 -0300

    Fixing ArgumentError: wrong number of arguments (given 2, expected 1) in recent ruby versions

--- config/initializers/enable_rack_on_sidekiq.rb
+++ config/initializers/enable_rack_on_sidekiq.rb
@@ -2,6 +2,6 @@ require 'kubernetes/health/rack_on_sidekiq'
 
 if Kubernetes::Health::Config.enable_rack_on_sidekiq && Kubernetes::Health::SidekiqOptionsResolver[:concurrency].positive?
   Thread.new do
-    Rack::Handler::WEBrick.run Kubernetes::Health::RackOnSidekiq.new, { Port:  Kubernetes::Health::Config.metrics_port }
+    Rack::Handler::WEBrick.run Kubernetes::Health::RackOnSidekiq.new, Port: Kubernetes::Health::Config.metrics_port
   end
 end
