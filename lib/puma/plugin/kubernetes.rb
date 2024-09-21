5ac229f83cb8acc5d45234e1fc85db395ecee5b9
Mark Delk <jethrodaniel@gmail.com>
2024-09-21 13:20:07 -0500

    fix a typo

--- lib/puma/plugin/kubernetes.rb
+++ lib/puma/plugin/kubernetes.rb
@@ -16,7 +16,7 @@ Puma::Plugin.create do
 
     case uri.scheme
     when 'tcp'
-      log_writer.log "* Starting kubernetes-healh server on #{str}"
+      log_writer.log "* Starting kubernetes-health server on #{str}"
       puma_server.add_tcp_listener uri.host, uri.port
     else
       log_writer.error "Invalid control URI: #{str}"
